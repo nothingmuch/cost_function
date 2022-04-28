89f0538eed8bc79f2a5b526573b04fa3cb71e033
Yuval Kogman <nothingmuch@woobling.org>
2022-04-28 17:28:24 +0000

    nix flake for exporting html & pdf from org
    Currently two packages are provided, and no default package is defined.
    A devshell with the combined dependencies is also provided, for use with
    `nix develop`.
    
    With additional org mode functionality and packages added later, emacs
    packages as well as external dependencies should be provided with nix so
    that the export is reproducible.
    
    Change-Id: I56ff890b3bdc21f897dd14a6a47f5bb1ab75dc1f

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "Bitcoin Cost Function Analaysis";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            git
+            git-review
+
+            emacs
+            texlive.combined.scheme-full
+          ];
+        };
+
+        # TODO refactor into something more modular
+        packages = {
+          pdf = pkgs.stdenvNoCC.mkDerivation rec {
+            name = "org latex export";
+            src = self;
+            buildInputs = [ pkgs.coreutils pkgs.emacs pkgs.texlive.combined.scheme-full ]; # TODO minimize tex dependencies
+            phases = ["unpackPhase" "buildPhase" "installPhase"];
+            buildPhase = ''
+              export PATH="${pkgs.lib.makeBinPath buildInputs}";
+              emacs -q index.org --batch -f org-latex-export-to-pdf --kill
+            '';
+            installPhase = ''
+              mkdir -p $out
+              cp index.pdf $out
+            '';
+          };
+
+          html = pkgs.stdenvNoCC.mkDerivation rec {
+            name = "org html export";
+            src = self;
+            buildInputs = [ pkgs.coreutils pkgs.emacs ];
+            phases = ["unpackPhase" "buildPhase" "installPhase"];
+            buildPhase = ''
+              export PATH="${pkgs.lib.makeBinPath buildInputs}";
+              emacs -q index.org --batch -f org-html-export-to-html --kill
+            '';
+            installPhase = ''
+              mkdir -p $out
+              cp index.html $out
+            '';
+          };
+        };
+      });
+}
